fd37518ae21205d1904b8cf0a404549c307d82ab
liliya <goldliliya@mail.ru>
2024-11-17 20:32:54 +0300

    OT-4: primevue, primeflex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css'
 
-createApp(App).mount('#app')
+async function mount() {
+    const app = createApp(App, {});
+    app.use(PrimeVue, {
+        theme: {
+            preset: Aura,
+            options: {
+                prefix: 'p',
+                darkModeSelector: '.my-app-light',
+                cssLayer: false
+            }
+        }
+    });
+
+    app.mount('#app');
+}
+
+void mount();
